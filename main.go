d10cc998b3039ee2ae3d42ed6010b1c1e434b159
MuhammadFahmii <fahmiprasetio754@gmail.com>
2021-09-21 10:45:17 +0700

    week5

--- main.go
+++ main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type User struct {
+	Name  string `json:"name" form:"name"`
+	Email string `json:"email" form:"email"`
+}
+
 func main() {
+	e := echo.New()
+	e.GET("/users", getUsers)
+	e.POST("/users", postUser)
+	e.Start(":8000")
+}
+
+func getUsers(c echo.Context) error {
+	user := User{Name: "udin", Email: "123@gmail.com"}
+	return c.JSON(http.StatusOK, user)
+}
+
+func postUser(c echo.Context) error {
+	newUser := User{}
+	c.Bind(&newUser)
+	return c.JSON(http.StatusCreated, map[string]interface{}{
+		"msg":  "Success create user",
+		"user": newUser,
+	})
 }
